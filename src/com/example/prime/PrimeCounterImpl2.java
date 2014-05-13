b130f734d77f9ae5e279686c3a02b9f5ce9ca47a
uryyyyyyy <koki305@gmail.com>
2014-05-14 07:42:25 +0900

    modify

--- src/com/example/prime/PrimeCounterImpl2.java
+++ src/com/example/prime/PrimeCounterImpl2.java
@@ -5,24 +5,30 @@ public class PrimeCounterImpl2 implements PrimeCounter{
 	/**
 	 * エラトステネスの篩を愚直に実装しました。
 	 * 配列にしたのはメモリに優しいからです。
-	 * 
+	 * ただ、1*10^9くらいのinputだと大量のメモリを消費します。
 	 * 
 	 */
 	@Override
 	public int countPrimeNumber(int inputNum) {
 		boolean[] primeList = new boolean[inputNum + 1];
 		int count = 0;
-		for (int i = 2; i <= inputNum; i++) {//1は素数でないので除外	
-			if (primeList[i] == false) {//未チェックのものがあれば、それは素数である。
+		double checkNumberLimit = Math.sqrt(inputNum);
+		for (int num = 2; num <= inputNum; num++) {//1は素数でないので除外	
+			if (primeList[num] == false) {//未チェックのものがあれば、それは素数である。
 				count++;
-				
-				//素数の倍数は素数でないのでチェック済み
-				for (int j = i; j <= inputNum; j += i) {
-					primeList[j] = true;
+				if(num <= checkNumberLimit){
+					mark(inputNum, primeList, num);
 				}
 			}
 		}
 		return count;
+	}
+
+	private void mark(int inputNum, boolean[] primeList, int i) {
+		//素数の倍数は素数でないのでチェック
+		for (int j = i; j <= inputNum; j += i) {
+			primeList[j] = true;
+		}
 	}
 
 }
